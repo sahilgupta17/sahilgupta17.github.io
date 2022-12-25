7d2b67b984fd2e49c68d6d21f467fd2cd7bc89c6
Sahil Gupta <sahgupta@umass.edu>
2022-12-25 00:12:09 -0500

    Updated skills sphere to use data from data folder

--- src/components/SkillsSphere/SkillsSphere.jsx
+++ src/components/SkillsSphere/SkillsSphere.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import TagCloud from "TagCloud";
+import SkillsData from "../../data/Skills.json";
 import "./SkillsSphere.module.css";
 
 // TODO: Update the css file. Remove redundant styling code
@@ -9,28 +10,7 @@ const SkillsSphere = () => {
   useEffect(() => {
     return () => {
       const container = ".tagcloud";
-      const skills = [
-        "Java",
-        "Python",
-        "JavaScript",
-        "TypeScript",
-        "Kotlin",
-        "SQL",
-        "C++",
-        "Selenium",
-        "AWS",
-        "AWS Lambda",
-        "AWS S3",
-        "AWS DynamoDB",
-        "Android Development",
-        "React Native",
-        "Firebase",
-        "NumPy",
-        "Pandas",
-        "Matplotlib",
-        "scikit-learn",
-        "Git",
-      ];
+      const skills = SkillsData.map((elem) => elem.name);
 
       const options = {
         radius: 300,
@@ -38,6 +18,7 @@ const SkillsSphere = () => {
         initSpeed: "normal",
         keep: true,
       };
+
       TagCloud(container, skills, options);
     };
   }, []);
