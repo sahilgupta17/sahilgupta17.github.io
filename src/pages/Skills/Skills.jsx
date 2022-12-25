7e06ff88c524b87393e8b7f80625ccdd0f7fd611
Sahil Gupta <sahgupta@umass.edu>
2022-12-24 22:24:32 -0500

    Adjusted skills sphere alignment

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SkillsSphere from "../../components/SkillsSphere/SkillsSphere";
+import styles from "./Skills.module.css";
 
 const Skills = () => {
   return (
-    <div>
-      <SkillsSphere />
+    <div className={styles.container}>
+      <div className={styles.skillsImageContainer}></div>
+      <SkillsSphere className={styles.skillsSphere} />
     </div>
   );
 };
