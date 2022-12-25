01ff0a5284840c4f5904e0726d70d755146f6038
Sahil Gupta <sahgupta@umass.edu>
2022-12-25 18:18:04 -0500

    Implemented Home Page with basic details and a background image

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,3 +1,5 @@
 // Home Page Images
 export { default as ProfessionalHeadshot } from "./webp/professional-headshot.webp";
 export { default as FallbackProfessionalHeadshot } from "./jpeg/professional-headshot.jpg";
+
+export { default as CaliforniaBackgroundImg } from "./jpeg/california.jpg";
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,43 @@
 import React from "react";
+import ImageHolder from "../../components/ImageHolder/ImageHolder";
+import {
+  ProfessionalHeadshot,
+  FallbackProfessionalHeadshot,
+  CaliforniaBackgroundImg,
+} from "../../assets/images";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import styles from "./Home.module.css";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div
+      className={styles.container}
+      style={{ background: `url(${CaliforniaBackgroundImg})` }}
+    >
+      <ImageHolder
+        primarySource={ProfessionalHeadshot}
+        secondarySource={FallbackProfessionalHeadshot}
+        alt="Professional Headshot of Sahil Gupta"
+        className={styles.professionalHeadshot}
+      />
+      <div className={styles.title}>
+        <span>Sahil Gupta</span>
+      </div>
+      <div className={styles.about}>
+        <span>Software Engineer, Mobile Developer</span>
+      </div>
+      <div className={styles.socials}>
+        <a href="https://www.linkedin.com/in/sahilgupta17" target="_blank">
+          <FaLinkedinIn className={styles.socialsIcon} />
+        </a>
+        <a href="https://github.com/sahilgupta17" target="_blank">
+          <FaGithub className={styles.socialsIcon} />
+        </a>
+        <a href="mailto:gsahil2000.14@gmail.com" target="_blank">
+          <MdEmail className={styles.socialsIcon} size={40} />
+        </a>
+      </div>
     </div>
   );
 };
