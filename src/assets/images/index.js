c057f9232bf73b01dbe7e94f7af314e4f340e7f0
Sahil Gupta <sahgupta@umass.edu>
2022-12-25 17:22:28 -0500

    Added professional headshot image to assets

---
+++ src/assets/images/index.js
@@ -0,0 +1,3 @@
+// Home Page Images
+export { default as ProfessionalHeadshot } from "./webp/professional-headshot.webp";
+export { default as FallbackProfessionalHeadshot } from "./jpeg/professional-headshot.jpg";
