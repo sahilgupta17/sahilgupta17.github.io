19a57eaef34bad8a5aecfc339e3e9481eca8de9d
Sahil Gupta <sahgupta@umass.edu>
2023-01-01 19:20:48 -0500

    Added timeline for experience

--- src/components/ExperienceCard/ExperienceCard.jsx
+++ src/components/ExperienceCard/ExperienceCard.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import "react-vertical-timeline-component/style.min.css";
 import styles from "./ExperienceCard.module.css";
 
 const ExperienceCard = (props) => {
   const description = props.data.description.map((item) => <li>{item}</li>);
+  const [displayDescription, setDisplayDescription] = React.useState(false);
   const skills = props.data.skills
-    .reduce((acc, item) => acc.concat(item + ", "), "")
-    .slice(0, -2);
+    .reduce((acc, item) => acc.concat(item + " • "), "")
+    .slice(0, -3);
+
+  const onDescriptionButtonClick = () => {
+    setDisplayDescription(!displayDescription);
+  };
 
   return (
     <div className={styles.container}>
@@ -19,12 +25,13 @@ const ExperienceCard = (props) => {
         <span className={styles.organization}>{props.data.organization}</span>
       </div>
       <span className={styles.title}>{props.data.title}</span>
-      {/* <span className={styles.duration}>{props.data.duration}</span> */}
       <span className={styles.location}>{props.data.location}</span>
       <span className={styles.skills}>Skills: {skills}</span>
-      <div className={styles.description}>
-        <ul>{description}</ul>
-      </div>
+      {displayDescription && (
+        <div className={styles.description}>
+          <ul>{description}</ul>
+        </div>
+      )}
       <div className={styles.footer}>
         <Button
           className={styles.websiteButton}
@@ -33,7 +40,12 @@ const ExperienceCard = (props) => {
         >
           Visit Website
         </Button>
-        <Button className={styles.descriptionButton}>View Description</Button>
+        <Button
+          className={styles.descriptionButton}
+          onClick={onDescriptionButtonClick}
+        >
+          View Description
+        </Button>
       </div>
     </div>
   );
--- src/pages/Experience/Experience.jsx
+++ src/pages/Experience/Experience.jsx
@@ -1,22 +1,56 @@
 import React from "react";
 import { ThemeContext } from "../../App";
 import ExperienceCard from "../../components/ExperienceCard/ExperienceCard";
+import { MdWork, MdWorkOutline } from "react-icons/md";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
 import ExperienceJSON from "../../data/Experience.json";
 import Header from "../../components/Header/Header";
+import "react-vertical-timeline-component/style.min.css";
 import styles from "./Experience.module.css";
 
 const Experience = () => {
   const { isDarkMode, toggleTheme } = React.useContext(ThemeContext);
+  const darkThemeContentArrowStyle = {
+    borderRight: "7px solid  rgb(48, 48, 48)",
+  };
+  const lightThemeContentArrowStyle = {
+    borderRight: "7px solid  rgb(249, 249, 249)",
+  };
 
   return (
     <div
       className={styles.container}
-      style={{ backgroundColor: isDarkMode ? "#121212" : "#f9f9f9" }}
+      style={{ backgroundColor: isDarkMode ? "#121212" : "#3da3d5" }}
     >
       <Header title="Experience" />
-      {ExperienceJSON.map((experience, index) => (
-        <ExperienceCard key={index} data={experience} />
-      ))}
+      <VerticalTimeline>
+        {ExperienceJSON.map((experience, index) => (
+          <VerticalTimelineElement
+            key={index}
+            date={experience.duration}
+            dateClassName="date"
+            icon={<MdWork />}
+            iconStyle={{
+              background: "#06D6A0",
+            }}
+            contentStyle={{
+              background: isDarkMode ? "#303030" : "#fff",
+              color: isDarkMode ? "white" : "black",
+              opacity: isDarkMode ? 0.8 : 1,
+            }}
+            contentArrowStyle={
+              isDarkMode
+                ? darkThemeContentArrowStyle
+                : lightThemeContentArrowStyle
+            }
+          >
+            <ExperienceCard key={index} data={experience} />
+          </VerticalTimelineElement>
+        ))}
+      </VerticalTimeline>
     </div>
   );
 };
