4103bdd2e86b0b60c074fdfb4cbe89b7667bbff0
Sahil Gupta <sahgupta@umass.edu>
2023-01-23 07:17:20 -0500

    Updated skill Icon shadow styles

--- src/components/SkillsIcon/SkillsIcon.jsx
+++ src/components/SkillsIcon/SkillsIcon.jsx
@@ -7,7 +7,12 @@ const SkillsIcon = (props) => {
   return (
     <div
       className={styles.container}
-      style={{ backgroundColor: isDarkMode ? "#303030" : "#fff" }}
+      style={{
+        backgroundColor: isDarkMode ? "#303030" : "#fff",
+        boxShadow: isDarkMode
+          ? "rgba(0, 0, 0, 0.8) 0px 5px 10px"
+          : "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
+      }}
     >
       <img
         className={styles.iconImg}
