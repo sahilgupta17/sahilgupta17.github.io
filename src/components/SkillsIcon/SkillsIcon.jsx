3f9368d4e91800ade11991bad6f2457ccaefa3f0
Sahil Gupta <sahgupta@umass.edu>
2023-01-23 08:10:07 -0500

    Updated skill icon background color

--- src/components/SkillsIcon/SkillsIcon.jsx
+++ src/components/SkillsIcon/SkillsIcon.jsx
@@ -8,9 +8,9 @@ const SkillsIcon = (props) => {
     <div
       className={styles.container}
       style={{
-        backgroundColor: isDarkMode ? "#303030" : "#fff",
+        backgroundColor: isDarkMode ? "#252525" : "#fff",
         boxShadow: isDarkMode
-          ? "rgba(0, 0, 0, 0.8) 0px 5px 10px"
+          ? "rgba(0, 0, 0, 0.8) 0px 7px 10px"
           : "rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px",
       }}
     >
